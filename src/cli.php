35f38d7b62fffca9517c05d86986a73ead22fcfc
Vladimir Moiseenko <mvaload@gmail.com>
2019-06-09 00:53:50 +0300

    add file cli files runing

---
+++ src/cli.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DiffCalc\cli;
+
+use function DiffCalc\differ\genDiff;
+
+const DOC = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help           Show this screen
+  --format <fmt>      Report format [default: pretty]
+DOC;
+
+function run()
+{
+    $args = \Docopt::handle(DOC);
+    $diff = genDiff($args['<firstFile>'], $args['<secondFile>'], $args['--format']);
+    print_r($diff);
+}
