1fdb0ecb40bf980c4eb582c16bda81c25bfaa03c
Vladimir Moiseenko <mvaload@gmail.com>
2019-06-10 23:40:35 +0300

    add file parser

---
+++ src/parser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DiffCalc\parser;
+
+use Symfony\Component\Yaml\Yaml;
+
+function getParser($content, $type)
+{
+    switch ($type) {
+        case 'json':
+            return json_decode($content, true);
+        case 'yaml':
+        case 'yml':
+            return Yaml::parse($content);
+    }
+}
+
+function parsedData($filePath)
+{
+    $type = pathinfo($filePath, PATHINFO_EXTENSION);
+    $content = file_get_contents($filePath);
+    $parse = getParser($content, $type);
+    return $parse;
+}
